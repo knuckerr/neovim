c002ae884ace75ada27f250ec7c2d78baf430d70
ioannis kalafatas <ioanniskalafatas@ioanniss-MacBook-Pro.local>
2025-10-19 14:10:14 +0300

    use lazy load

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,120 +1,130 @@
 local M = {}
 
 function M.setup()
-  return require('packer').startup(function()
-      
-    -- Packer can manage itself as an optional plugin
+  return require('packer').startup(function(use)
+    -- Packer can manage itself
     use {'wbthomason/packer.nvim', opt = true}
 
-    -- Dracula theme
-    use {'https://github.com/Mofiqul/dracula.nvim'}
+    -- Dracula theme (small, load immediately)
+    use {'Mofiqul/dracula.nvim'}
 
-    -- Auto pair
+    -- Auto pair (load immediately)
     use {
       "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
+      config = function() require("nvim-autopairs").setup {} end
     }
 
-    -- Fuzzy finder
+    -- Telescope (lazy on command)
     use {
       'nvim-telescope/telescope.nvim',
-    -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+      requires = { {'nvim-lua/plenary.nvim'} },
+      cmd = 'Telescope',
+      config = function()
+        require('telescope').setup {
+          defaults = {
+            prompt_prefix = "🔍 ",
+            selection_caret = "➤ ",
+            path_display = {"smart"},
+          }
+        }
+      end
     }
 
-    use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
-    use {'junegunn/fzf.vim'}
+    -- FZF (lazy on command)
+    use {'junegunn/fzf', dir = '~/.fzf', run = './install --all', cmd = 'FZF'}
+    use {'junegunn/fzf.vim', cmd = {'FZF', 'Files', 'Buffers'}}
 
+    -- File explorer (lazy on command)
     use {
       "kyazdani42/nvim-tree.lua",
+      cmd = {"NvimTreeToggle", "NvimTreeOpen"},
       config = function()
         require("nvim-tree").setup {}
-      end,
-    }
-
-
-    -- LSP and completion
-    use {
-    "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
       end
     }
 
+    -- Mason & LSP (lazy on BufReadPre)
     use {
-      'williamboman/mason-lspconfig.nvim',
-      config = function()
-        require("mason-lspconfig").setup()
-      end
+      "williamboman/mason.nvim",
+      event = "BufReadPre",
+      config = function() require("mason").setup() end
     }
-
+    use {
+      "williamboman/mason-lspconfig.nvim",
+      event = "BufReadPre",
+      config = function() require("mason-lspconfig").setup() end
+    }
     use {
       'neovim/nvim-lspconfig',
-      config = function()
-        require('config.lspconfig').setup()
-      end,
+      event = "BufReadPre",
+      config = function() require('config.lspconfig').setup() end
     }
-    -- Collection of configurations for built-in LSP client
-    use {'hrsh7th/nvim-cmp'} -- Autocompletion plugin
-    use {'hrsh7th/cmp-buffer'} -- Autocompletion plugin
-    use {'hrsh7th/cmp-nvim-lsp'} -- LSP source for nvim-cmp
-    use {'saadparwaiz1/cmp_luasnip'} -- Snippets source for nvim-cmp
-    use {'L3MON4D3/LuaSnip'} -- Snippets plugin
-    use {'nvim-lua/plenary.nvim'}
-    use {'nvim-treesitter/nvim-treesitter'}
 
+    -- Completion & snippets (lazy on InsertEnter)
+    use {'hrsh7th/nvim-cmp', event = "InsertEnter"}
+    use {'hrsh7th/cmp-buffer', event = "InsertEnter"}
+    use {'hrsh7th/cmp-nvim-lsp', event = "InsertEnter"}
+    use {'saadparwaiz1/cmp_luasnip', event = "InsertEnter"}
+    use {'L3MON4D3/LuaSnip', event = "InsertEnter"}
 
+    -- Treesitter (lazy on buffer read)
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate',
+      event = 'BufRead',
+      config = function()
+        require('nvim-treesitter.configs').setup {
+          highlight = { enable = true },
+          indent = { enable = true },
+        }
+      end
+    }
 
-    -- Vim dispatch
-    use { 'tpope/vim-dispatch' }
+    -- Git
+    use { 'tpope/vim-dispatch', event = 'BufReadPost' }
     use {
       'airblade/vim-gitgutter',
+      event = 'BufReadPost',
       config = function()
-	require('config.git-gutter').setup()
-      end,
+        require('config.git-gutter').setup()
+      end
     }
+    use {'tpope/vim-fugitive', cmd = {'G','Git','Gblame'}}
 
-    -- Syntax & indent
-    use 'sheerun/vim-polyglot'
+    -- Syntax & indentation
+    use {'sheerun/vim-polyglot', event = 'BufRead'}
 
+    -- Icons (load immediately)
+    use {"kyazdani42/nvim-web-devicons"}
 
-    -- Fugitive for Git
-    use { 'tpope/vim-fugitive' }
-
-    -- Icons 
-    use {
-      "kyazdani42/nvim-web-devicons",
-    }
-
-    -- status line
+    -- Status line (lazy on VimEnter)
     use {
       "feline-nvim/feline.nvim",
-       config = function()
-         require('feline').setup()
-        end,
+      event = "VimEnter",
+      config = function() require('feline').setup() end
     }
-    -- dashboard
+
+    -- Dashboard (lazy on VimEnter)
     use {
       'glepnir/dashboard-nvim',
       event = 'VimEnter',
       config = function()
-        require('dashboard').setup {
-          -- config
-        }
+        require('dashboard').setup {}
       end,
-      requires = {'nvim-tree/nvim-web-devicons'}
+      requires = {'kyazdani42/nvim-web-devicons'}
     }
 
-    -- hop
+    -- Hop (lazy on keypress 's' or 'S')
     use {
       'phaazon/hop.nvim',
-      branch = 'v2', -- optional but strongly recommended
+      branch = 'v2',
+      keys = {'s','S'},
       config = function()
-        -- you can configure Hop the way you like here; see :h hop-config
         require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
       end
     }
 
   end)
 end
+
 return M
