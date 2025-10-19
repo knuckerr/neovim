153f187dc8283f447f59289c4212ed64720e7567
ioannis kalafatas <ioanniskalafatas@ioanniss-MacBook-Pro.local>
2025-10-19 14:48:54 +0300

    add more bindings

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -21,6 +21,22 @@ function M.setup()
   --------------------
   keymap("n", "<leader>a", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
   keymap("n", "<leader>u", "<cmd>UndotreeToggle<CR>", { desc = "Toggle undo tree" })
+
+
+  -- Buffers (SPC b)
+  keymap("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { desc = "List buffers" })
+  keymap("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete current buffer" })
+
+  -- Git (SPC g)
+  keymap("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Git status" })
+  keymap("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Git commit" })
+  keymap("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push" })
+  keymap("n", "<leader>gl", "<cmd>Git pull<CR>", { desc = "Git pull" })
+  keymap("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Git blame" })
+
+  -- Terminal (SPC t)
+  keymap("n", "<leader>tt", "<cmd>split | terminal<CR>", { desc = "Open terminal (split)" })
+  keymap("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", { desc = "Open terminal (vsplit)" })
 end
 
 return M
